2f37d0ef5989765d359202303abda1f21a261015
SalmaanMushtaq <87965170+SalmaanMushtaq@users.noreply.github.com>
2021-09-30 11:37:48 +0530

    Add files via upload

---
+++ Array-sum.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int AddElements(int *ptr, int size)
+{
+  int total = 0;
+  for( int i = 0; i<size; i++)
+    {
+      total = total + ptr[i];
+    }
+  return total;
+}
+
+
+int  main()
+{
+  int arr[] = { 1, 3 , 4, 5, 6, 10, 15, 17, 18, 25, 15 };
+  int size = sizeof arr/sizeof arr[0];
+ 
+  printf("Sum of  array is= %d\n", AddElements(arr,size));
+ }
