b8eacc87f23efd2bf6672ce04424dbfce3c1d2d1
SalmaanMushtaq <87965170+SalmaanMushtaq@users.noreply.github.com>
2021-09-30 14:33:24 +0530

    Add files via upload

---
+++ math-functions.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<math.h>
+float main()
+{
+  printf("power of 3 and 4 = %f\n", pow(3,4));
+  printf("Square root of 81 = %f\n", sqrt(81));
+  printf("Absolute vale of -98.0 = %f\n", fabs(-98.0));
+  printf("Sin of 0 = %f\n", sin(0) );
+  printf("COs of 0 = %f\n", cos(0));
+  printf("Tan of 0 = %f\n", tan(0));
+  printf("Ceil of 36.5 = %f\n", ceil(36.5));
+  printf("floor of 36.5 = %f\n", floor(36.5));
+
+  printf("Sin inverse of 0= %f\n", asin(0));
+
+  printf("Cos inverse of 0 = %f\n", acos(0));
+
+  printf("Tan inverse of 0 = %f\n", atan(0));
+
+  printf("Log of 10 = %f\n", log10(10.0));
+
+  printf("Exponenet of e to the power 6 = %f\n", exp(6.0));
+
+}
