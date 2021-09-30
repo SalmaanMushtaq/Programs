1cf6d2a106cb2779588d6dfada484111a12587db
SalmaanMushtaq <87965170+SalmaanMushtaq@users.noreply.github.com>
2021-09-30 11:35:59 +0530

    Add files via upload

---
+++ arraysum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int total = 0;
+  int arr[]= { 1, 2, 3, 4, 5, 6, 7};
+  int size = sizeof arr/sizeof arr[0];
+  printf("Size is %i\n", size);
+  for(int i=0; i< size; i++)
+    total=total+ arr[i];
+  printf("Sum is =  %d\n ", total);
+}
